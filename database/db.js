0c2c43a0874bcdbc8bb5b2cef8436974ec8363aa
Yash Bhavsar <yashbhavsar175@gmail.com>
2025-06-16 14:20:43 +0530

    _

--- database/db.js
+++ database/db.js
@@ -1,9 +1,15 @@
-const {Client} = require ('pg');
+import 'dotenv/config';
+import pkg from 'pg';
+const { Client } = pkg;
 
-const client = new Client({
-	host:'localhost',
-	database:'test',
-	port:5432,
-	username:'postgres',
-	password:'Yash2003@'
-})
+const conn = new Client({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+conn.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Connection error', err.stack));
